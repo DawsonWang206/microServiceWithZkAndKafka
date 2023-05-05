fcd22c05023bc778bed9ad67cbab216a0770c7f7
DawsonWang <wdt985@live.cn>
2023-05-05 17:34:20 +0800

    add kefka_temp module

---
+++ kafka_temp/src/main/java/com/dawson/kafka_temp/KafkaTempApp.java
@@ -0,0 +1,19 @@
+package com.dawson.kafka_temp;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KafkaTempApp {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTempApp.class, args);
+    }
+    @Bean
+    public ApplicationRunner runner(){
+        return args -> {
+            System.out.println("启动成功");
+        };
+    }
+}
